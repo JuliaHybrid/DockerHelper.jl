2031d74e56b9b572bd428093aa1ba99d36ef46dc
overshiki <le.niu@hotmail.com>
2023-01-28 14:46:06 +0800

    add Path

--- src/DockerHelper.jl
+++ src/DockerHelper.jl
@@ -6,6 +6,6 @@ export Maintainer, Run, Copy, EnvAppend,
         Location, Apt, Conda, Pip, JuliaAdd, 
         CopyOut, run_copy, run_copyout, run_build,
         run_clean, run_retag, run_push,
-        Builder
+        Builder, Path
 
 end # module DockerHelper
